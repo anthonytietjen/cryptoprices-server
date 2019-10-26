3cd88ff81e247996437412e07bebbf94ef462606
Anthony Tietjen <tietjen.anthony@gmail.com>
2019-10-26 00:58:10 -0600

    Getting multiple crypto prices

--- handler.js
+++ handler.js
@@ -1,23 +1,39 @@
-'use strict';
 const axios = require('axios')
 
 module.exports.hello = async event => {
-  const response = await axios.get('https://api.coinbase.com/v2/prices/spot?currency=USD')
-  const prices = response.data
+  const [btcResponse, ltcResponse, etcReponse] = await Promise.all([
+    axios.get('https://api.coinbase.com/v2/prices/BTC-USD/buy'),
+    axios.get('https://api.coinbase.com/v2/prices/LTC-USD/buy'),
+    axios.get('https://api.coinbase.com/v2/prices/ETH-USD/buy')
+  ])
+
+  const cryptos = [
+    {
+      id: 'btc',
+      name: 'Bitcoin',
+      price: btcResponse.data.data.amount
+    },
+    {
+      id: 'ltc',
+      name: 'Litecoin',
+      price: ltcResponse.data.data.amount
+    },
+    {
+      id: 'eth',
+      name: 'Ethereum',
+      price: etcReponse.data.data.amount
+    }
+  ]
+
+  const body = {
+    cryptos
+  }
+
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-control-allow-origin": "*"
     },
-    body: JSON.stringify(
-      {
-        prices
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    body: JSON.stringify(body),
+  }
 };
