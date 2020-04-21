1f39e3f7fd8416b6c0c5bae7eb57819661d6bcfc
Anthony Tietjen <tietjen.anthony@gmail.com>
2020-04-20 20:41:08 -0600

    Added /v1/savePricePoints

--- handler.js
+++ handler.js
@@ -1,6 +1,13 @@
 const axios = require('axios')
 
 module.exports.hello = async event => {
+  switch (event.resource) {
+    case "/v1/prices": return await getPrices(event)
+    case "/v1/savePricePoints": return await savePricePoints(event)
+  }
+};
+
+const getPrices = async (event) => {
   const [btcResponse, ltcResponse, etcReponse] = await Promise.all([
     axios.get('https://api.coinbase.com/v2/prices/BTC-USD/buy'),
     axios.get('https://api.coinbase.com/v2/prices/LTC-USD/buy'),
@@ -25,8 +32,18 @@ module.exports.hello = async event => {
     }
   ]
 
+  const pricePoints = {
+    btcSaved: 1,
+    btcBuffer: 2,
+    ltcSaved: 3,
+    ltcBuffer: 4,
+    ethSaved: 5,
+    ethBuffer: 6,
+  }
+
   const body = {
-    cryptos
+    cryptos,
+    pricePoints
   }
 
   return {
@@ -36,4 +53,14 @@ module.exports.hello = async event => {
     },
     body: JSON.stringify(body),
   }
-};
+}
+
+const savePricePoints = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-control-allow-origin": "*"
+    },
+    body: JSON.stringify({ "success": true }),
+  }
+}
